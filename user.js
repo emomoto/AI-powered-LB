7d4a3a8ca7253dc13380d41ae1bf17dd762b3227
emomoto <163348701+emomoto@users.noreply.github.com>
2024-08-27 14:09:58 +0200

    Add user.js

---
+++ user.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
