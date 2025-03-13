e85907f5cbcb18f4bb06b1d4edf308b9e0a96b14
emomoto <163348701+emomoto@users.noreply.github.com>
2025-03-13 11:56:51 +0100

    error catching update

--- auth.js
+++ auth.js
@@ -3,11 +3,23 @@ require('dotenv').config();
 
 const authenticateToken = (req, res, next) => {
   const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
-  if (token == null) return res.sendStatus(401);
+  if (!authHeader) {
+    return res.status(401).json({ error: "Authorization header is missing" });
+  }
+
+  const token = authHeader.split(' ')[1];
+  if (!token) {
+    return res.status(401).json({ error: "Bearer token is missing" });
+  }
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403);
+    if (err) {
+      const message = err.name === 'JsonWebTokenError' ? "Invalid token" : 
+                      err.name === 'TokenExpiredError' ? "Token expired" : 
+                      "Token not valid";
+
+      return res.status(403).json({ error: message });
+    }
     req.user = user;
     next();
   });
